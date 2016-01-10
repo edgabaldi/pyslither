b1fadbac0a19d1996fda61962fb22d7db4ac8e03
Edgar Gabaldi <edgabaldi@gmail.com>
2016-01-10 13:56:55 -0200

    specifying other default column types

--- pyslither/column.py
+++ pyslither/column.py
@@ -1,9 +1,15 @@
+import datetime
+from decimal import Decimal
+
+MONEY_WITH_IMPLIED_DECIMAL = 'MONEY_WITH_IMPLIED_DECIMAL'
+
 class Column:
 
-    def __init__(self, name, length, type=str):
+    def __init__(self, name, length, type=str, **options):
         self.name = name
         self.length = length
         self.type = type
+        self.options = options
 
     def parse(self, value):
         if self.type is int:
@@ -12,4 +18,22 @@ class Column:
             except ValueError:
                 value = 0
             return value
+        elif self.type is Decimal:
+            try:
+                value = Decimal(value)
+            except:
+                value = Decimal(0)
+            return value
+        elif self.type is MONEY_WITH_IMPLIED_DECIMAL:
+            try:
+                value = Decimal(value)
+            except:
+                value = Decimal(0)
+            return value / 100
+        elif self.type is datetime.date:
+            if 'format' in self.options.keys():
+                format = self.options['format']
+            else:
+                format = '%Y-%m-%d'
+            return datetime.datetime.strptime(value, format).date()
         return value.strip()
--- tests/test_column.py
+++ tests/test_column.py
@@ -1,6 +1,8 @@
+import datetime
+from decimal import Decimal
 from unittest import TestCase
 
-from pyslither.column import Column
+from pyslither.column import Column, MONEY_WITH_IMPLIED_DECIMAL
 
 
 class ColumnTestCase(TestCase):
@@ -21,16 +23,29 @@ class ColumnTestCase(TestCase):
         self.assertEqual(0, column.parse('Ryan'))
         self.assertEqual(23, column.parse('00023.45'))
 
-    def test_parse_should_support_float_type(self):
-        pass
+    def test_parse_should_support_decimal_type(self):
+        column = Column('amount', 10, type=Decimal)
+
+        self.assertEqual(Decimal('234.45'), column.parse('  234.45'))
+        self.assertEqual(Decimal('234.56'), column.parse('234.5600'))
+        self.assertEqual(Decimal('234'), column.parse('   234'))
+        self.assertEqual(Decimal('234'), column.parse('0000234'))
+        self.assertEqual(Decimal('0'), column.parse('Ryan'))
+        self.assertEqual(Decimal('23.45'), column.parse('00023.45'))
 
     def test_parse_should_support_money_with_implied_decimal_type(self):
-        pass
+        column = Column('amount', 10, type=MONEY_WITH_IMPLIED_DECIMAL)
+        self.assertEqual(Decimal('234.45'),column.parse('   23445'))
 
     def test_parse_should_support_date_type(self):
-        pass
+        column = Column('date', 10, type=datetime.date)
+        date = column.parse('2016-01-10')
+        self.assertEqual(datetime.date(2016, 1, 10), date)
+        self.assertTrue(isinstance(date, datetime.date))
 
     def test_parse_should_use_format_option_with_date_type_if_avaliable(self):
-        pass
+        column = Column('date', 10, type=datetime.date, format='%d/%m/%Y')
+        date = column.parse('10/01/2016')
+        self.assertEqual(datetime.date(2016, 1, 10), date)
 
 
